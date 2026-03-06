7aca13716233d4a7f9e0065a5d3d764a5341a333
Alicorn-Max-S <maxjacobsaraf@gmail.com>
2026-03-05 22:31:42 -0800

    Made stuff work
    soooo basically now the code actually looks at your answers and does stuff based on that wooooo I am the best coder ever.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function thisDoesSomething () {
-    console.log("BROOOOO GET THIS")
-    document.getElementById("yay").innerHTML = "heyyyy"
+const answers = ["18", "8"];
+
+let questionsAnswered = 0;
+
+function checkAccuracy(id) {
+  var input = document.getElementById(`q${id}`).value;
+  console.log(document.getElementById(`q${id}`).value);
+  console.log(answers[id - 1]);
+  if (input.toLowerCase() == answers[id - 1].toLowerCase()) {
+    document.getElementById(`q${id}Result`).innerHTML = "that is correct :)";
+  } else {
+    document.getElementById(`q${id}Result`).innerHTML =
+      `that is incorrect :( the correct answer is ${answers[id - 1]}`;
+  }
+  questionsAnswered++;
+
+  if (questionsAnswered == 2) {
+    document.getElementById("teaching1").innerHTML =
+      "Because they have eight valence electrons, what is their common ion? Discuss.";
+  }
 }
