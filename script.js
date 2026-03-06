489a935e0c31fb33445e1ee4cc1d0bc664f026c1
Alicorn-Max <maxjacobsaraf@gmail.com>
2026-03-06 14:39:16 -0800

    Added questions
    Added questions + improved logic

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-const answers = ["18", "8"];
+const answers = ["18", "8", "false", "true", "false", "false", "false", "true", ["full valence shell"], ['radon', 'xenon'], ["none"], "Radon can cause cancer, however symptoms from exposure may not be noticed for years. "];
 
 let questionsAnswered = 0;
 
-function checkAccuracy(id) {
+function checkAccuracy(id, contains = false) {
   var input = document.getElementById(`q${id}`).value;
-  console.log(document.getElementById(`q${id}`).value);
-  console.log(answers[id - 1]);
-  if (input.toLowerCase() == answers[id - 1].toLowerCase()) {
-    document.getElementById(`q${id}Result`).innerHTML = "that is correct :)";
-  } else {
-    document.getElementById(`q${id}Result`).innerHTML =
-      `that is incorrect :( the correct answer is ${answers[id - 1]}`;
+  
+  if (contains) {
+    if (answers[id - 1].some(substring=>input.toLowerCase().includes(substring))) {
+      document.getElementById(`q${id}Result`).innerHTML = "that is correct :)";
+      questionsAnswered++;
+    } 
+    else {
+      document.getElementById(`q${id}Result`).innerHTML =
+        `that is incorrect :( the correct answer contains ${answers[id - 1].toString()}`;
+    }
+  } 
+  else {
+    if (input.toLowerCase().includes(answers[id - 1].toLowerCase())) {
+      document.getElementById(`q${id}Result`).innerHTML = "that is correct :)";
+      questionsAnswered++;
+    } 
+    else {
+      document.getElementById(`q${id}Result`).innerHTML =
+        `that is incorrect :( the correct answer is ${answers[id - 1]}`;
+    }
   }
-  questionsAnswered++;
 
   if (questionsAnswered == 2) {
-    document.getElementById("teaching1").innerHTML =
-      "Because they have eight valence electrons, what is their common ion? Discuss.";
+    document.getElementById("teaching1").style.opacity = "1"
+    document.getElementById("teaching2").style.opacity = "1"
+    document.getElementById("group2").style.opacity = "1"
+  }
+
+  if (questionsAnswered == 8) {
+    document.getElementById("group3").style.opacity = "1"
+    document.getElementById("teaching3").style.opacity = "1"
   }
 }
